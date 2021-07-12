26f5657c1d458ed171772a33061185d432e9aed7
psmith150 <patricksmith150@gmail.com>
2021-07-11 21:37:11 -0500

    Added weapon summary to combat screen. Added additional tests

--- tests/combat_tests.py
+++ tests/combat_tests.py
@@ -1,6 +1,38 @@
 import unittest
 from toolbox.combat import Damage, WeaponType, Weapon, WeaponAttack, Dice, DamageType
 
+class DamageTypeTestCase(unittest.TestCase):
+    def test_get_values(self):
+        values = DamageType.get_values()
+        self.assertEqual(len(values), len(list(DamageType)))
+    
+    def test_convert_display_name(self):
+        val = DamageType(1)
+        self.assertEqual(DamageType.convert_display_name(DamageType.get_display_name(val)), val)
+
+class DiceTestCase(unittest.TestCase):
+    def test_get_values(self):
+        values = Dice.get_values()
+        self.assertEqual(len(values), len(list(Dice)))
+    
+    def test_convert_display_name(self):
+        val = Dice(1)
+        self.assertEqual(Dice.convert_display_name(Dice.get_display_name(val)), val)
+
+class DamageTestCase(unittest.TestCase):
+    def test_average(self):
+        damage = Damage(1, Dice.D6, DamageType.PIERCING)
+        self.assertAlmostEqual(damage.average(), 3.5)
+
+class WeaponTypeTestCase(unittest.TestCase):
+    def test_get_values(self):
+        values = WeaponType.get_values()
+        self.assertEqual(len(values), len(list(WeaponType)))
+    
+    def test_convert_display_name(self):
+        val = WeaponType(1)
+        self.assertEqual(WeaponType.convert_display_name(WeaponType.get_display_name(val)), val)
+
 class WeaponTestCase(unittest.TestCase):
     def test_average_damage(self):
         weapon = Weapon(WeaponType.WARHAMMER)
@@ -18,13 +50,7 @@ class WeaponTestCase(unittest.TestCase):
         weapon = Weapon(WeaponType.MACE, bonus=1, extra_damage=[Damage(1, Dice.D6, DamageType.BLUDGEONING),])
         self.assertAlmostEqual(weapon.average_damage(), 8)
 
-class AttackTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        weapon = Weapon(WeaponType.WARHAMMER)
-        self.hammer_attack = WeaponAttack(weapon, 5, 18)
-        weapon = Weapon(WeaponType.MACE, 1, [Damage(1, Dice.D6, DamageType.BLUDGEONING),])
-        self.mace_attack = WeaponAttack(weapon, 5, 18)
-    
+class AttackTestCase(unittest.TestCase):    
     def test_hit_chance(self):
         weapon = Weapon(WeaponType.WARHAMMER)
         weapon_attack = WeaponAttack(weapon, 5, 18)
@@ -35,6 +61,26 @@ class AttackTestCase(unittest.TestCase):
         weapon_attack = WeaponAttack(weapon, 5, 18)
         self.assertEqual(weapon_attack.hit_chance(11), 18)
 
+    def test_average_hit_damage(self):
+        weapon = Weapon(WeaponType.WARHAMMER)
+        weapon_attack = WeaponAttack(weapon, 5, 18)
+        self.assertAlmostEqual(weapon_attack.average_hit_damage(), 8.5)
+    
+    def test_average_hit_damage_bonus(self):
+        weapon = Weapon(WeaponType.MACE, bonus=1)
+        weapon_attack = WeaponAttack(weapon, 5, 18)
+        self.assertAlmostEqual(weapon_attack.average_hit_damage(), 8.5)
+    
+    def test_average_hit_damage_extra_damage(self):
+        weapon = Weapon(WeaponType.MACE, extra_damage=[Damage(1, Dice.D6, DamageType.BLUDGEONING),])
+        weapon_attack = WeaponAttack(weapon, 5, 18)
+        self.assertAlmostEqual(weapon_attack.average_hit_damage(), 11.0)
+    
+    def test_average_hit_damage_bonus_and_extra_damage(self):
+        weapon = Weapon(WeaponType.MACE, bonus=1, extra_damage=[Damage(1, Dice.D6, DamageType.BLUDGEONING),])
+        weapon_attack = WeaponAttack(weapon, 5, 18)
+        self.assertAlmostEqual(weapon_attack.average_hit_damage(), 12.0)
+    
     def test_average_damage(self):
         weapon = Weapon(WeaponType.WARHAMMER)
         weapon_attack = WeaponAttack(weapon, 5, 18)
@@ -54,3 +100,23 @@ class AttackTestCase(unittest.TestCase):
         weapon = Weapon(WeaponType.MACE, bonus=1, extra_damage=[Damage(1, Dice.D6, DamageType.BLUDGEONING),])
         weapon_attack = WeaponAttack(weapon, 5, 18)
         self.assertAlmostEqual(weapon_attack.average_damage(11), 11.15)
+    
+    def test_critical_hit_damage(self):
+        weapon = Weapon(WeaponType.WARHAMMER)
+        weapon_attack = WeaponAttack(weapon, 5, 18)
+        self.assertAlmostEqual(weapon_attack.critical_hit_damage(), 13.0)
+    
+    def test_critical_hit_damage_bonus(self):
+        weapon = Weapon(WeaponType.MACE, bonus=1)
+        weapon_attack = WeaponAttack(weapon, 5, 18)
+        self.assertAlmostEqual(weapon_attack.critical_hit_damage(), 12.0)
+    
+    def test_critical_hit_damage_extra_damage(self):
+        weapon = Weapon(WeaponType.MACE, extra_damage=[Damage(1, Dice.D6, DamageType.BLUDGEONING),])
+        weapon_attack = WeaponAttack(weapon, 5, 18)
+        self.assertAlmostEqual(weapon_attack.critical_hit_damage(), 18.0)
+    
+    def test_critical_hit_damage_bonus_and_extra_damage(self):
+        weapon = Weapon(WeaponType.MACE, bonus=1, extra_damage=[Damage(1, Dice.D6, DamageType.BLUDGEONING),])
+        weapon_attack = WeaponAttack(weapon, 5, 18)
+        self.assertAlmostEqual(weapon_attack.critical_hit_damage(), 19.0)
--- toolbox/toolbox.py
+++ toolbox/toolbox.py
@@ -24,10 +24,12 @@ HIT_BONUS_KEY = 'hit-bonus'
 AVG_HIT_DAMAGE_KEY = 'avg-hit-damage'
 AVG_DAMAGE_KEY = 'avg-damage'
 WEAPON_DAMAGE_FRAME_KEY = 'weapon-damage-frame'
+WEAPON_SUMMARY_KEY = 'weapon-damage-summary'
 
 DAMAGE_CALCULATION_EVENTS = [CHARACTER_LEVEL_KEY, CHARACTER_ATTACK_STAT_KEY,
     CHARACTER_DAMAGE_MOD_KEY, TARGET_AC_KEY, WEAPON_TYPE_KEY, WEAPON_BONUS_KEY,
-    DICE_NUMBER_KEY, DAMAGE_DIE_KEY, DAMAGE_TYPE_KEY, PROFICIENCY_KEY]
+    DICE_NUMBER_KEY, DAMAGE_DIE_KEY, DAMAGE_TYPE_KEY, PROFICIENCY_KEY,
+    REMOVE_WEAPON_DAMAGE_BUTTON_KEY, ADD_WEAPON_DAMAGE_BUTTON_KEY]
 
 NUM_DAMAGE_PANELS = 3
 
@@ -41,7 +43,7 @@ def main():
             event, values = window.read(timeout=10)
         else:
             event, values = window.read()
-        print(event, values)
+        #print(event, values)
         if (not first_read):
             first_read = True
             init_combat_panel(window, values)
@@ -85,7 +87,9 @@ def combat_panel() -> sg.Column:
         [character_panel()],
         [weapon_panel(1), weapon_panel(2)],
         [sg.HorizontalSeparator()],
-        [sg.Frame('Results', layout=[[weapon_result_panel(1), sg.VerticalSeparator(), weapon_result_panel(2)]])]
+        [sg.Frame('Results', layout=[
+            [weapon_result_panel(1), sg.VerticalSeparator(), weapon_result_panel(2)],
+            [sg.Text('Weapon 2 deals equal damage to Weapon 1',key=WEAPON_SUMMARY_KEY, size=(40,1), justification='center')]])]
     ]
 
     return sg.Column(layout)
@@ -114,22 +118,26 @@ def character_panel() -> sg.Column:
     return sg.Column(layout, expand_x=True, element_justification='center')
 
 def weapon_panel(index: int) -> sg.Column:
+    weapon_values = WeaponType.get_values()
+    weapon_values.sort()
     layout = [[sg.Frame(f'Weapon {index}', layout=[
                 [sg.Text('Weapon type:', size=(15,1), justification='left'),
-                    sg.Combo(WeaponType.get_values(),
-                        default_value=WeaponType.get_display_name(WeaponType.CLUB),
-                        key=WEAPON_TYPE_KEY + f"-{index}", enable_events=True,
+                    sg.Combo(weapon_values,
+                        default_value=WeaponType.get_display_name(WeaponType.BATTLEAXE),
+                        key=f'{WEAPON_TYPE_KEY}-{index}', enable_events=True,
                         readonly=True, size=(20, 1))],
                 [sg.Text('Bonus:', size=(15,1), justification='left'),
                     sg.Spin([i for i in range(0, 10)], initial_value=0,
-                        key=WEAPON_BONUS_KEY + f"-{index}", enable_events=True,
+                        key=f'{WEAPON_BONUS_KEY}-{index}', enable_events=True,
                         size=(5,1), auto_size_text=False)],
                 [sg.Text('Proficient?', size=(15,1), justification='left'),
-                    sg.Checkbox('', key=PROFICIENCY_KEY + f'-{index}',
+                    sg.Checkbox('', key=f'{PROFICIENCY_KEY}-{index}',
                         enable_events=True, default=True)]
                 ])],
-        [sg.Column(layout=[[sg.Button('Add', key=ADD_WEAPON_DAMAGE_BUTTON_KEY + f'-{index}')]]),
-            sg.Column(layout=[[sg.Button('Remove', key=REMOVE_WEAPON_DAMAGE_BUTTON_KEY + f'-{index}', visible=False)]])],
+        [sg.Column(layout=[[sg.Button('Add Damage', key=f'{ADD_WEAPON_DAMAGE_BUTTON_KEY}-{index}',
+                size=(12,1))]]),
+            sg.Column(layout=[[sg.Button('Remove Damage',
+                key=f'{REMOVE_WEAPON_DAMAGE_BUTTON_KEY}-{index}', visible=False, size=(12,1))]])],
     ]
     damage_panels = []
     for panel in range(NUM_DAMAGE_PANELS):
@@ -145,16 +153,16 @@ def weapon_damage_panel(parent_index: int, index: int, visible: bool = False) ->
     layout = [
         [sg.Text('Number of dice:', size=(15,1), justification='left'),
             sg.Spin([i for i in range(0, 20)], initial_value=0,
-                key=DICE_NUMBER_KEY + f"-{parent_index}-{index}",
+                key=f'{DICE_NUMBER_KEY}-{parent_index}-{index}',
                 enable_events=True, size=(5,1), auto_size_text=False)],
         [sg.Text('Damage die:', size=(15,1), justification='left'),
             sg.Combo(Dice.get_values(), default_value=Dice.get_display_name(Dice.D6),
-                key=DAMAGE_DIE_KEY + f"-{parent_index}-{index}", enable_events=True,
+                key=f'{DAMAGE_DIE_KEY}-{parent_index}-{index}', enable_events=True,
                 readonly=True, size=(5,1))],
         [sg.Text('Damage type:', size=(15,1), justification='left'),
             sg.Combo(DamageType.get_values(),
                 default_value=DamageType.get_display_name(DamageType.ACID),
-                key=DAMAGE_TYPE_KEY + f"-{parent_index}-{index}",
+                key=f'{DAMAGE_TYPE_KEY}-{parent_index}-{index}',
                 enable_events=True, readonly=True, size=(12,1))]
     ]
 
@@ -164,11 +172,11 @@ def weapon_damage_panel(parent_index: int, index: int, visible: bool = False) ->
 def weapon_result_panel(parent_index: int) -> sg.Column:
     layout = [
         [sg.Text('Hit Bonus:', size=(20,1)),
-            sg.Text('0', key=HIT_BONUS_KEY + f"-{parent_index}", size=(10,1))],
+            sg.Text('0', key=f'{HIT_BONUS_KEY}-{parent_index}', size=(10,1))],
         [sg.Text('Average Damage on Hit:', size=(20,1)),
-            sg.Text('0', key=AVG_HIT_DAMAGE_KEY + f"-{parent_index}", size=(10,1))],
+            sg.Text('0', key=f'{AVG_HIT_DAMAGE_KEY}-{parent_index}', size=(10,1))],
         [sg.Text('Average Damage to Target:', size=(20,1)),
-            sg.Text('0', key=AVG_DAMAGE_KEY + f"-{parent_index}", size=(10,1))]
+            sg.Text('0', key=f'{AVG_DAMAGE_KEY}-{parent_index}', size=(10,1))]
     ]
 
     return sg.Column(layout, size=(291, 78), pad=((5, 18), (5,5)))
@@ -178,41 +186,44 @@ def add_weapon_damage(window: sg.Window, parent_index: int):
     window[f'{WEAPON_DAMAGE_FRAME_KEY}-{parent_index}'].update(visible=True)
     window[f'{WEAPON_DAMAGE_FRAME_KEY}-{parent_index}'].unhide_row()
     next_index = -1
-    parent_col = window[WEAPON_PANEL_KEY + f'-{parent_index}']
+    parent_col = window[f'{WEAPON_PANEL_KEY}-{parent_index}']
     for index in range(NUM_DAMAGE_PANELS):
-        col = window[WEAPON_DAMAGE_PANEL_KEY + f'-{parent_index}-{index+1}']
+        col = window[f'{WEAPON_DAMAGE_PANEL_KEY}-{parent_index}-{index+1}']
         if (not col.visible):
             next_index = index
             break
     if next_index < 0:
-        window[ADD_WEAPON_DAMAGE_BUTTON_KEY + f'-{parent_index}'].update(visible=False)
+        window[f'{ADD_WEAPON_DAMAGE_BUTTON_KEY}-{parent_index}'].update(visible=False)
         return
-    col = window[WEAPON_DAMAGE_PANEL_KEY + f'-{parent_index}-{next_index+1}']
+    col = window[f'{WEAPON_DAMAGE_PANEL_KEY}-{parent_index}-{next_index+1}']
     col.unhide_row()
     col.update(visible=True)
-    window[REMOVE_WEAPON_DAMAGE_BUTTON_KEY + f'-{parent_index}'].update(visible=True)
+    window[f'{DICE_NUMBER_KEY}-{parent_index}-{next_index+1}'].update(value=0)
+    window[f'{DAMAGE_DIE_KEY}-{parent_index}-{next_index+1}'].update(value=Dice.get_display_name(Dice.D6))
+    window[f'{DAMAGE_TYPE_KEY}-{parent_index}-{next_index+1}'].update(value=DamageType.get_display_name(DamageType.ACID))
+    window[f'{REMOVE_WEAPON_DAMAGE_BUTTON_KEY}-{parent_index}'].update(visible=True)
     if (next_index == NUM_DAMAGE_PANELS - 1):
-        window[ADD_WEAPON_DAMAGE_BUTTON_KEY + f'-{parent_index}'].update(visible=False)
+        window[f'{ADD_WEAPON_DAMAGE_BUTTON_KEY}-{parent_index}'].update(visible=False)
     window.refresh()
 
 def remove_weapon_damage(window: sg.Window, parent_index: int):
     # Find last visible panel
     next_index = -1
-    parent_col = window[WEAPON_PANEL_KEY + f'-{parent_index}']
+    parent_col = window[f'{WEAPON_PANEL_KEY}-{parent_index}']
     for index in range(NUM_DAMAGE_PANELS, 0, -1):
-        col = window[WEAPON_DAMAGE_PANEL_KEY + f'-{parent_index}-{index}']
+        col = window[f'{WEAPON_DAMAGE_PANEL_KEY}-{parent_index}-{index}']
         if (col.visible):
             next_index = index
             break
     if next_index < 0:
-        window[REMOVE_WEAPON_DAMAGE_BUTTON_KEY + f'-{parent_index}'].update(visible=False)
+        window[f'{REMOVE_WEAPON_DAMAGE_BUTTON_KEY}-{parent_index}'].update(visible=False)
         return
-    col = window[WEAPON_DAMAGE_PANEL_KEY + f'-{parent_index}-{next_index}']
+    col = window[f'{WEAPON_DAMAGE_PANEL_KEY}-{parent_index}-{next_index}']
     col.hide_row()
     col.update(visible=False)
-    window[ADD_WEAPON_DAMAGE_BUTTON_KEY + f'-{parent_index}'].update(visible=True)
+    window[f'{ADD_WEAPON_DAMAGE_BUTTON_KEY}-{parent_index}'].update(visible=True)
     if (next_index == 1):
-        window[REMOVE_WEAPON_DAMAGE_BUTTON_KEY + f'-{parent_index}'].update(visible=False)
+        window[f'{REMOVE_WEAPON_DAMAGE_BUTTON_KEY}-{parent_index}'].update(visible=False)
         window[f'{WEAPON_DAMAGE_FRAME_KEY}-{parent_index}'].hide_row()
         window[f'{WEAPON_DAMAGE_FRAME_KEY}-{parent_index}'].update(visible=False)
     window.refresh()
@@ -222,10 +233,12 @@ def update_weapon_attack(window: sg.Window, values: dict, index: int):
     bonus = int(values[f'{WEAPON_BONUS_KEY}-{index}'])
     extra_damage = []
     for damage_index in range(NUM_DAMAGE_PANELS):
-        num_dice = int(values[f'{DICE_NUMBER_KEY}-{index}-{damage_index+1}'])
-        damage_die = Dice.convert_display_name(values[f'{DAMAGE_DIE_KEY}-{index}-{damage_index+1}'])
-        damage_type = DamageType.convert_display_name(values[f'{DAMAGE_TYPE_KEY}-{index}-{damage_index+1}'])
-        extra_damage.append(Damage(num_dice, damage_die, damage_type))
+        col = window[f'{WEAPON_DAMAGE_PANEL_KEY}-{index}-{damage_index+1}']
+        if (col.visible):
+            num_dice = int(values[f'{DICE_NUMBER_KEY}-{index}-{damage_index+1}'])
+            damage_die = Dice.convert_display_name(values[f'{DAMAGE_DIE_KEY}-{index}-{damage_index+1}'])
+            damage_type = DamageType.convert_display_name(values[f'{DAMAGE_TYPE_KEY}-{index}-{damage_index+1}'])
+            extra_damage.append(Damage(num_dice, damage_die, damage_type))
     weapon = Weapon(weapon_type, bonus, extra_damage)
     level = int(values[CHARACTER_LEVEL_KEY])
     attack_stat = int(values[CHARACTER_ATTACK_STAT_KEY])
@@ -240,6 +253,26 @@ def update_weapon_attack(window: sg.Window, values: dict, index: int):
     window[f'{HIT_BONUS_KEY}-{index}'].update(value=hit_bonus)
     window[f'{AVG_HIT_DAMAGE_KEY}-{index}'].update(value='{:.2f}'.format(avg_hit_damage))
     window[f'{AVG_DAMAGE_KEY}-{index}'].update(value='{:.2f}'.format(avg_damage))
+    event, values = window.read(timeout=0)
+    update_weapon_summary(window, values)
+
+def update_weapon_summary(window, values):
+    weapon_damages = []
+    for weapon_index in range(1,3):
+        weapon_damages.append(float(window[f'{AVG_DAMAGE_KEY}-{weapon_index}'].get()))
+    if (abs(weapon_damages[1]) < 0.00001):
+        difference = 0.0
+    else:
+        difference = (weapon_damages[0] - weapon_damages[1]) / weapon_damages[1]
+    if abs(difference) < 0.00001:
+        summary = f'Weapon 1 deals the same damage as weapon 2'
+    elif (difference < 0):
+        summary = f'Weapon 1 deals {"{:0.2%}".format(difference)} less damage than weapon 2'
+    else:
+        summary = f'Weapon 1 deals {"{:0.2%}".format(difference)} more damage than weapon 2'
+    window[WEAPON_SUMMARY_KEY].update(value = summary)
+    window[WEAPON_SUMMARY_KEY].expand(expand_x = True)
+
 
 def init_combat_panel(window: sg.Window, values: dict):
     for update_index in range(1,3):
