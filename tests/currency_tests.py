3579ecdd58e56fbb305373371533e89a145160cc
psmith150 <patricksmith150@gmail.com>
2021-07-12 21:49:00 -0500

    Expanded currency splitting and consolidating. Added currency tests

--- tests/currency_tests.py
+++ tests/currency_tests.py
@@ -1,5 +1,5 @@
-from unittest import TestCase
-from toolbox.currency import Currency
+from unittest import TestCase, main
+from toolbox.currency import Currency, CurrencyOptions
 
 class CurrencyTestCase(TestCase):
     def test_equal(self):
@@ -32,8 +32,56 @@ class CurrencyTestCase(TestCase):
         cur2 = Currency(1, 2, 3, 4, 0)
         self.assertNotEqual(cur1, cur2)
     
-    def test_consolidate(self):
-        cur1 = Currency(101, 201, 301, 401, 501).consolidate()
+    def test_to_copper(self):
+        cur1 = Currency(1, 2, 3, 4, 5)
+        self.assertEqual(cur1.to_copper(), 1395)
+    
+    def test_consolidate_all(self):
+        cur1 = Currency(101, 201, 301, 401, 501).consolidate(CurrencyOptions.ALL)
         cur2 = Currency(140, 6, 1, 1, 1)
         self.assertEqual(cur1, cur2)
     
+    def test_consolidate_noplatinum(self):
+        options = CurrencyOptions.COPPER | CurrencyOptions.SILVER | CurrencyOptions.ELECTRUM | CurrencyOptions.GOLD
+        cur1 = Currency(101, 201, 301, 401, 501).consolidate(options)
+        cur2 = Currency(0, 1406, 1, 1, 1)
+        self.assertEqual(cur1, cur2)
+    
+    def test_consolidate_nogold(self):
+        options = CurrencyOptions.COPPER | CurrencyOptions.SILVER | CurrencyOptions.ELECTRUM | CurrencyOptions.PLATINUM
+        cur1 = Currency(101, 201, 301, 401, 501).consolidate(options)
+        cur2 = Currency(140, 0, 13, 1, 1)
+        self.assertEqual(cur1, cur2)
+    
+    def test_consolidate_noelectrum(self):
+        options = CurrencyOptions.COPPER | CurrencyOptions.SILVER | CurrencyOptions.GOLD | CurrencyOptions.PLATINUM
+        cur1 = Currency(101, 201, 301, 401, 501).consolidate(options)
+        cur2 = Currency(140, 6, 0, 6, 1)
+        self.assertEqual(cur1, cur2)
+    
+    def test_consolidate_nosilver(self):
+        options = CurrencyOptions.COPPER | CurrencyOptions.ELECTRUM | CurrencyOptions.GOLD | CurrencyOptions.PLATINUM
+        cur1 = Currency(101, 201, 301, 401, 501).consolidate(options)
+        cur2 = Currency(140, 6, 1, 0, 11)
+        self.assertEqual(cur1, cur2)
+    
+    def test_split_evenly_consolidate(self):
+        currencies = Currency(100, 100, 100, 100, 100).split(2)
+        self.assertEqual(currencies[0], currencies[1])
+        self.assertEqual(currencies[0], Currency(gold=580, silver=5))
+    
+    def test_split_unevenly_consolidate(self):
+        currencies = Currency(100, 100, 100, 100, 101).split(2)
+        self.assertEqual(currencies[0], Currency(gold=580, silver=5, copper=1))
+        self.assertEqual(currencies[1], Currency(gold=580, silver=5))
+    
+    def test_split_evenly_noconsolidate(self):
+        currencies = Currency(100, 100, 100, 100, 100).split(2, False)
+        self.assertEqual(currencies[0], currencies[1])
+        self.assertEqual(currencies[0], Currency(50, 50, 50, 50, 50))
+    
+    def test_split_unevenly_noconsolidate(self):
+        currencies = Currency(3, 2, 5, 5, 5).split(3, False)
+        self.assertEqual(currencies[0], Currency(1, 1, 1, 2, 0))
+        self.assertEqual(currencies[1], Currency(1, 1, 1, 2, 0))
+        self.assertEqual(currencies[2], Currency(1, 0, 3, 1, 5))
--- toolbox/currency.py
+++ toolbox/currency.py
@@ -1,6 +1,18 @@
 from __future__ import annotations
 from typing import List
+from functools import total_ordering
+from enum import Flag, auto
 
+class CurrencyOptions(Flag):
+    COPPER = 0
+    SILVER = auto()
+    ELECTRUM = auto()
+    GOLD = auto()
+    PLATINUM = auto()
+    COMMON = COPPER | SILVER | GOLD
+    ALL = COPPER | SILVER | ELECTRUM | GOLD | PLATINUM
+
+@total_ordering
 class Currency():
     def __init__(self, platinum: int = 0, gold: int = 0, electrum: int = 0, silver: int = 0, copper: int = 0) -> None:
         self.platinum = platinum
@@ -19,30 +31,68 @@ class Currency():
         equal &= self.copper == o.copper
         return equal
     
-    def consolidate(self, noplatinum: bool=True, nogold: bool=False, noelectrum: bool=True, nosilver: bool=False) -> Currency:
+    def __lt__(self, o:object) -> bool:
+        if (not isinstance(o,Currency)):
+            return NotImplemented
+        return self.to_copper() < o.to_copper()
+    
+    def to_copper(self) -> int:
+        return self.platinum * 1000 + self.gold * 100 + self.electrum * 50 + self.silver * 10 + self.copper
+    
+    def consolidate(self, currencies: CurrencyOptions=CurrencyOptions.COMMON) -> Currency:
+        copper = self.to_copper()
         cur = Currency()
-        cur.silver = self.silver + self.copper // 10
-        cur.copper = self.copper % 10
-        cur.electrum = self.electrum + cur.silver // 5
-        cur.silver = cur.silver % 5
-        cur.gold = self.gold + cur.electrum // 2
-        cur.electrum = cur.electrum % 2
-        cur.platinum = self.platinum + cur.gold // 10
-        cur.gold = cur.gold % 10
-
+        if (CurrencyOptions.PLATINUM in currencies):
+            cur.platinum = copper // 1000
+            copper %= 1000
+        if (CurrencyOptions.GOLD in currencies):
+            cur.gold = copper // 100
+            copper %= 100
+        if (CurrencyOptions.ELECTRUM in currencies):
+            cur.electrum = copper // 50
+            copper %= 50
+        if (CurrencyOptions.SILVER in currencies):
+            cur.silver = copper // 10
+            copper %= 10
+        cur.copper = copper
         return cur
     
-    def split(self, players: int, consolidate: bool=True) -> List[Currency]:
+    def split(self, players: int, consolidate: bool=True, consolidate_currencies: CurrencyOptions=CurrencyOptions.COMMON) -> List[Currency]:
         if (consolidate):
-            copper = self.platinum * 1000 + self.gold * 100 + self.electrum * 50 + self.silver * 10 + self.copper
+            copper = self.to_copper()
             remainder = copper % players
             currencies = []
             for index in range(remainder):
-                currencies.append(Currency(copper=copper // players + 1).consolidate())
+                currencies.append(Currency(copper=copper // players + 1).consolidate(consolidate_currencies))
             for index in range(players - remainder):
-                currencies.append(Currency(copper=copper // players).consolidate())
+                currencies.append(Currency(copper=copper // players).consolidate(consolidate_currencies))
         else:
-            currencies = []
+            coin = self.platinum
+            currencies = [Currency() for i in range(players)]
+            while (coin > 0):
+                min_cur = min(currencies)
+                min_cur.platinum += 1
+                coin -= 1
+            coin = self.gold
+            while (coin > 0):
+                min_cur = min(currencies)
+                min_cur.gold += 1
+                coin -= 1
+            coin = self.electrum
+            while (coin > 0):
+                min_cur = min(currencies)
+                min_cur.electrum += 1
+                coin -= 1
+            coin = self.silver
+            while (coin > 0):
+                min_cur = min(currencies)
+                min_cur.silver += 1
+                coin -= 1
+            coin = self.copper
+            while (coin > 0):
+                min_cur = min(currencies)
+                min_cur.copper += 1
+                coin -= 1
         return currencies
 
         
