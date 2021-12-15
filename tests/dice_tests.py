0bc9d624fb8f44d43f41b149abac2ded7f0b7f8e
psmith150 <patricksmith150@gmail.com>
2021-12-14 22:17:48 -0600

    Added all dice rolling functionality to DiceRoll class. Began outlining methods for DiceCollection class.

--- tests/dice_tests.py
+++ tests/dice_tests.py
@@ -1,27 +1,207 @@
 """Test the implementation of the dice.py module."""
 from unittest import TestCase, main
 from toolbox.common import Dice
-from toolbox.dice import DieRoll
+from toolbox.dice import DiceRoll, DiceCollection, SpecialRoll
 
-class DieRollTestCase(TestCase):
+class DiceRollTestCase(TestCase):
+    def test_get_min_value_no_modifier(self):
+        roll = DiceRoll(Dice.D100, 1)
+        self.assertEqual(roll.min_value(), 1)
+    
+    def test_get_min_value_multiple_dice_no_modifier(self):
+        roll = DiceRoll(Dice.D100, 2)
+        self.assertEqual(roll.min_value(), 2)
+    
+    def test_get_min_value_static_modifier(self):
+        roll = DiceRoll(Dice.D100, 1, 10)
+        self.assertEqual(roll.min_value(), 11)
+    
+    def test_get_min_value_multiple_dice_static_modifier(self):
+        roll = DiceRoll(Dice.D100, 2, 10)
+        self.assertEqual(roll.min_value(), 12)
+    
+    def test_get_min_value_drop_lowest(self):
+        roll = DiceRoll(Dice.D100, 2, 0, SpecialRoll.DROP_LOWEST, 1)
+        self.assertEqual(roll.min_value(), 1)
+    
+    def test_get_min_value_drop_highest(self):
+        roll = DiceRoll(Dice.D100, 2, 0, SpecialRoll.DROP_HIGHEST, 1)
+        self.assertEqual(roll.min_value(), 1)
+    
+    def test_get_max_value_no_modifier(self):
+        roll = DiceRoll(Dice.D100, 1)
+        self.assertEqual(roll.max_value(), 100)
+    
+    def test_get_max_value_multiple_dice_no_modifier(self):
+        roll = DiceRoll(Dice.D100, 2)
+        self.assertEqual(roll.max_value(), 200)
+    
+    def test_get_max_value_static_modifier(self):
+        roll = DiceRoll(Dice.D100, 1, 10)
+        self.assertEqual(roll.max_value(), 110)
+    
+    def test_get_max_value_multiple_dice_static_modifier(self):
+        roll = DiceRoll(Dice.D100, 2, 10)
+        self.assertEqual(roll.max_value(), 210)
+    
+    def test_get_max_value_drop_lowest(self):
+        roll = DiceRoll(Dice.D100, 2, 0, SpecialRoll.DROP_LOWEST, 1)
+        self.assertEqual(roll.max_value(), 100)
+    
+    def test_get_max_value_drop_highest(self):
+        roll = DiceRoll(Dice.D100, 2, 0, SpecialRoll.DROP_HIGHEST, 1)
+        self.assertEqual(roll.max_value(), 100)
+    
+    def test_get_average_value_no_modifier(self):
+        roll = DiceRoll(Dice.D100, 1)
+        self.assertAlmostEqual(roll.average_value(), 50.5)
+    
+    def test_get_average_value_multiple_dice_no_modifier(self):
+        roll = DiceRoll(Dice.D100, 2)
+        self.assertAlmostEqual(roll.average_value(), 101)
+    
+    def test_get_average_value_static_modifier(self):
+        roll = DiceRoll(Dice.D100, 1, 10)
+        self.assertAlmostEqual(roll.average_value(), 60.5)
+    
+    def test_get_average_value_multiple_dice_static_modifier(self):
+        roll = DiceRoll(Dice.D100, 2, 10)
+        self.assertAlmostEqual(roll.average_value(), 111)
+    
+    def test_get_average_value_drop_lowest(self):
+        roll = DiceRoll(Dice.D6, 2, 0, SpecialRoll.DROP_LOWEST, 1)
+        self.assertAlmostEqual(roll.average_value(), 161/36)
+    
+    def test_get_average_value_drop_highest(self):
+        roll = DiceRoll(Dice.D6, 2, 0, SpecialRoll.DROP_HIGHEST, 1)
+        self.assertAlmostEqual(roll.average_value(), 91/36)
+
+    def test_get_all_values_one_die(self):
+        roll = DiceRoll(Dice.D6)
+        values = roll.get_all_values()
+        values.sort()
+        self.assertEqual(values.count(1), 1)
+        self.assertEqual(values.count(2), 1)
+        self.assertEqual(values.count(3), 1)
+        self.assertEqual(values.count(4), 1)
+        self.assertEqual(values.count(5), 1)
+        self.assertEqual(values.count(6), 1)
+    
+    def test_get_all_values_two_die(self):
+        roll = DiceRoll(Dice.D6, 2)
+        values = roll.get_all_values()
+        values.sort()
+        self.assertEqual(values.count(2), 1)
+        self.assertEqual(values.count(3), 2)
+        self.assertEqual(values.count(4), 3)
+        self.assertEqual(values.count(5), 4)
+        self.assertEqual(values.count(6), 5)
+        self.assertEqual(values.count(7), 6)
+        self.assertEqual(values.count(8), 5)
+        self.assertEqual(values.count(9), 4)
+        self.assertEqual(values.count(10), 3)
+        self.assertEqual(values.count(11), 2)
+        self.assertEqual(values.count(12), 1)
+
+    def test_get_probability_target_single_die(self):
+        roll = DiceRoll(Dice.D100, 1)
+        self.assertAlmostEqual(roll.get_probability_target(50), 1/100)
+    
+    def test_get_probability_target_single_die_static_modifier(self):
+        roll = DiceRoll(Dice.D100, 1, 1)
+        self.assertAlmostEqual(roll.get_probability_target(50), 1/100)
+    
+    def test_get_probability_target_two_dice(self):
+        roll = DiceRoll(Dice.D6, 2)
+        self.assertAlmostEqual(roll.get_probability_target(8), 5/36)
+    
+    def test_get_probability_target_two_dice_and_static_modifier(self):
+        roll = DiceRoll(Dice.D6, 2, 1)
+        self.assertAlmostEqual(roll.get_probability_target(9), 5/36)
+    
+    def test_get_probability_target_two_dice_drop_lowest(self):
+        roll = DiceRoll(Dice.D100, 2, 0, SpecialRoll.DROP_LOWEST, 1)
+        self.assertAlmostEqual(roll.get_probability_target(50), 99/10000)
+    
+    def test_get_probability_target_two_dice_drop_highest(self):
+        roll = DiceRoll(Dice.D100, 2, 0, SpecialRoll.DROP_HIGHEST, 1)
+        self.assertAlmostEqual(roll.get_probability_target(50), 101/10000)
+    
+    def test_get_probability_range_single_die(self):
+        roll = DiceRoll(Dice.D100, 1)
+        self.assertAlmostEqual(roll.get_probability_range(51, 100), 50/100)
+    
+    def test_get_probability_range_single_die_static_modifier(self):
+        roll = DiceRoll(Dice.D100, 1, 1)
+        self.assertAlmostEqual(roll.get_probability_range(51, 100), 50/100)
+    
+    def test_get_probability_range_two_dice(self):
+        roll = DiceRoll(Dice.D6, 2)
+        self.assertAlmostEqual(roll.get_probability_range(8, 10), 12/36)
+    
+    def test_get_probability_range_two_dice_and_static_modifier(self):
+        roll = DiceRoll(Dice.D6, 2, 1)
+        self.assertAlmostEqual(roll.get_probability_range(8, 10), 15/36)
+    
+    def test_get_probability_range_two_dice_drop_lowest(self):
+        roll = DiceRoll(Dice.D100, 2, 0, SpecialRoll.DROP_LOWEST, 1)
+        self.assertAlmostEqual(roll.get_probability_range(49, 51), 297/10000)
+    
+    def test_get_probability_range_two_dice_drop_highest(self):
+        roll = DiceRoll(Dice.D100, 2, 0, SpecialRoll.DROP_HIGHEST, 1)
+        self.assertAlmostEqual(roll.get_probability_range(49, 51), 303/10000)
+    
+    def test_get_all_combinations_single_die(self):
+        roll = DiceRoll(Dice.D4, 1)
+        combinations = roll.get_all_combinations()
+        combinations.sort()
+        self.assertEqual(combinations, [[1], [2], [3], [4]])
+    
+    def test_get_all_combinations_single_die_modifier(self):
+        roll = DiceRoll(Dice.D4, 1, 1)
+        combinations = roll.get_all_combinations()
+        combinations.sort()
+        self.assertEqual(combinations, [[1], [2], [3], [4]])
+    
+    def test_get_all_combinations_two_dice(self):
+        roll = DiceRoll(Dice.D4, 2)
+        combinations = roll.get_all_combinations()
+        combinations.sort()
+        self.assertEqual(combinations, [
+            [1, 1], [1, 2], [1, 3], [1, 4],
+            [2, 1], [2, 2], [2, 3], [2, 4],
+            [3, 1], [3, 2], [3, 3], [3, 4],
+            [4, 1], [4, 2], [4, 3], [4, 4]])
+    
+    def test_get_all_combinations_two_dice_modifier(self):
+        roll = DiceRoll(Dice.D4, 2, 1)
+        combinations = roll.get_all_combinations()
+        combinations.sort()
+        self.assertEqual(combinations, [
+            [1, 1], [1, 2], [1, 3], [1, 4],
+            [2, 1], [2, 2], [2, 3], [2, 4],
+            [3, 1], [3, 2], [3, 3], [3, 4],
+            [4, 1], [4, 2], [4, 3], [4, 4]])
+
+class DiceCollectionTestCase(TestCase):
     def test_get_min_value_no_modifiers(self):
-        roll = DieRoll(Dice.D100)
+        roll = DiceCollection(Dice.D100)
         self.assertEqual(roll.min_value(), 1)
     
     def test_get_min_value_static_modifiers(self):
-        roll = DieRoll(Dice.D100, [10])
+        roll = DiceCollection(Dice.D100, [10])
         self.assertEqual(roll.min_value(), 11)
     
     def test_get_min_value_dynamic_modifiers(self):
-        roll = DieRoll(Dice.D100, [Dice.D4])
+        roll = DiceCollection(Dice.D100, [Dice.D4])
         self.assertEqual(roll.min_value(), 2)
     
     def test_get_min_value_mixed_modifiers(self):
-        roll = DieRoll(Dice.D100, [Dice.D4, 10])
+        roll = DiceCollection(Dice.D100, [Dice.D4, 10])
         self.assertEqual(roll.min_value(), 12)
 
     def test_get_all_values_one_die(self):
-        roll = DieRoll(Dice.D100)
+        roll = DiceCollection(Dice.D100)
         values = roll.get_all_values([Dice.D6])
         values.sort()
         self.assertEqual(values.count(1), 1)
@@ -32,7 +212,7 @@ class DieRollTestCase(TestCase):
         self.assertEqual(values.count(6), 1)
     
     def test_get_all_values_two_same_die(self):
-        roll = DieRoll(Dice.D100)
+        roll = DiceCollection(Dice.D100)
         values = roll.get_all_values([Dice.D6, Dice.D6])
         values.sort()
         self.assertEqual(values.count(2), 1)
@@ -48,7 +228,7 @@ class DieRollTestCase(TestCase):
         self.assertEqual(values.count(12), 1)
     
     def test_get_all_values_two_different_die(self):
-        roll = DieRoll(Dice.D100)
+        roll = DiceCollection(Dice.D100)
         values = roll.get_all_values([Dice.D6, Dice.D2])
         values.sort()
         self.assertEqual(values.count(2), 1)
@@ -60,17 +240,17 @@ class DieRollTestCase(TestCase):
         self.assertEqual(values.count(8), 1)
 
     def test_get_probability_single_die(self):
-        roll = DieRoll(Dice.D100)
+        roll = DiceCollection(Dice.D100)
         self.assertAlmostEqual(roll.get_probability(50), 1/100)
     
     def test_get_probablility_single_die_static_modifier(self):
-        roll = DieRoll(Dice.D100, [1])
+        roll = DiceCollection(Dice.D100, [1])
         self.assertAlmostEqual(roll.get_probability(50), 1/100)
     
     def test_get_probability_two_dice(self):
-        roll = DieRoll(Dice.D6, [Dice.D6])
+        roll = DiceCollection(Dice.D6, [Dice.D6])
         self.assertAlmostEqual(roll.get_probability(8), 5/36)
     
     def test_get_probability_two_dice_and_static_modifier(self):
-        roll = DieRoll(Dice.D6, [Dice.D6, 1])
+        roll = DiceCollection(Dice.D6, [Dice.D6, 1])
         self.assertAlmostEqual(roll.get_probability(9), 5/36)
--- toolbox/dice.py
+++ toolbox/dice.py
@@ -5,82 +5,340 @@ Classes:
 """
 from __future__ import absolute_import, annotations
 import collections
+from enum import Enum, auto
 from .common import Dice
+from math import ceil
 
-class DieRoll():
+class SpecialRoll(Enum):
+    NONE = auto()
+    DROP_LOWEST = auto()
+    DROP_HIGHEST = auto()
+    # TODO REPLACE_LOWER = auto()
+    # TODO REPLACE_HIGHER = auto()
+
+class DiceRoll():
+    """Represents a roll of a number of same-sided dice.
     """
-    Represents a roll of a die.
-    """
-    def __init__(self, die: Dice, modifiers: list = [], advantage: bool = False) -> None:
-        """Initializes the DieRoll object with the given parameters.
+    def __init__(self, die: Dice, number: int = 1, modifier: int = 0,
+        special: SpecialRoll = SpecialRoll.NONE, special_value: int = 0) -> None:
+        """Initializes the DiceRoll object with the given parameters.
 
         Args:
             die (Dice): The die to use for the base roll
-            modifiers (list): Additional modifiers to add to the roll. Defaults to empty.
-            advantage (bool, optional): Whether or not to roll with advantage. Defaults to False.
+            number (int, optional): The number of dice to roll. Defaults to 1.
+            modifier (int, optional): An additional modifier to add to the roll. Defaults to 0.
+            special (SpecialRoll, optional): Special modifiers to the roll. Defaults to NONE.
+            special_value (int, optional): The value to apply to the selected special. Defaults to 0.
         """
         self.die = die
-        self.modifiers = modifiers
-        self.advantage = advantage
+        self.number = int(number) if int(number) > 0 else 1
+        self.modifier = int(modifier)
+        self.special = special
+        if special_value >= number:
+            self.special_value = number - 1
+        elif special_value < 0:
+            self.special_value = 0
+        else:
+            self.special_value = special_value
     
     def min_value(self) -> int:
+        """Calculates the minimum possible value of the dice roll.
+
+        Returns:
+            int: The mimimum possible value of the dice roll.
+        """
         base_val = 0 if self.die == Dice.D0 else 1
-        mod_val = 0
-        for mod in self.modifiers:
-            if isinstance(mod, Dice):
-                mod_val += 0 if mod == Dice.D0 else 1
-            elif isinstance(mod, int):
-                mod_val += mod
-            else:
-                return NotImplemented
-        return base_val + mod_val
-                
+        if (self.special == SpecialRoll.NONE):
+            return base_val * self.number + self.modifier 
+        elif (self.special == SpecialRoll.DROP_LOWEST):
+            return base_val * (self.number - self.special_value) + self.modifier  
+        elif (self.special == SpecialRoll.DROP_HIGHEST):
+            return base_val * (self.number - self.special_value) + self.modifier 
+        else:
+            return NotImplemented 
 
     def max_value(self):
-        base_val = self.die.value
-        mod_val = 0
-        for mod in self.modifiers:
-            if isinstance(mod, Dice):
-                mod_val += mod.value
-            elif isinstance(mod, int):
-                mod_val += mod
-            else:
-                return NotImplemented
-        return base_val + mod_val
+        """Calculates the maximum possible value of the dice roll.
+
+        Returns:
+            int: The maximum possible value of the dice roll.
+        """
+        if (self.special == SpecialRoll.NONE):
+            return self.die.value * self.number + self.modifier 
+        elif (self.special == SpecialRoll.DROP_LOWEST):
+            return self.die.value * (self.number - self.special_value) + self.modifier  
+        elif (self.special == SpecialRoll.DROP_HIGHEST):
+            return self.die.value * (self.number - self.special_value) + self.modifier 
+        else:
+            return NotImplemented 
     
-    def get_probability(self, target: int) -> float:
+    def average_value(self) -> float:
+        """Calculates the average value of the dice roll.
+
+        Returns:
+            float: The average value of the dice roll.
+        """
+        values = self.get_all_values()
+        values = [v + self.modifier for v in values]
+        return sum(values) / len(values)
+    
+    def get_probability_target(self, target: int) -> float:
+        """Calculates the probability of the dice roll totaling a given value.
+
+        Args:
+            target (int): The target value.
+
+        Returns:
+            float: The probability of totaling the target value.
+        """
         if target < self.min_value() or target > self.max_value():
             return 0.0
-        
-        # Subtract all static modifiers
-        adjusted_target = target
-        for modifier in self.modifiers:
-            if (isinstance(modifier, int) and not isinstance(modifier, Dice)):
-                adjusted_target -= modifier
-        possible_values = []
-        dice_modifiers = [mod for mod in self.modifiers if isinstance(mod, Dice)]
-        for base_val in range(1, self.die.value + 1):
-            possible_values += [base_val + return_val for return_val in self.get_all_values(dice_modifiers)]
+        probabilities = self.get_all_probabilities()
+        return probabilities[target]
+    
+    def get_probability_range(self, min_value: int, max_value: int) -> float:
+        """Calculates the probability of the dice roll landing within a given range.
+
+        Args:
+            min_value (int): The minimum value of the range (inclusive).
+            max_value (int): The maximum value of the range (exclusive).
+
+        Returns:
+            float: The probability of the total roll being within the range.
+        """
+        if min_value < self.min_value() or max_value > self.max_value() or min_value > max_value:
+            return 0.0
+        probabilities = self.get_all_probabilities()
+        return sum([probabilities[p] for p in probabilities.keys() if min_value <= p <= max_value])
+    
+    def get_all_probabilities(self) -> dict[int, float]:
+        """Calculates the probability of rolling each possible value.
+
+        Returns:
+            dict[int, float]: A dictionary where they keys are possible die rolls and the values are the probability of the roll.
+        """
+        possible_values = self.get_all_values()
+        possible_values = [v + self.modifier for v in possible_values]
         total_possible_outcomes = len(possible_values)
         outcomes = collections.Counter(possible_values)
-        target_outcomes = outcomes[adjusted_target]
-        return target_outcomes / total_possible_outcomes
+        return {p:outcomes[p] / total_possible_outcomes for p in range(self.min_value(), self.max_value() + 1)}
     
-    def get_all_values(self, dice: list) -> list:
+    def get_all_values(self) -> list[int]:
+        """Gets all possible values of the dice roll, not considering the modifier.
+        List of all values will include all duplicates.
+
+        Returns:
+            list[int]: A list of all possible dice rolls.
+        """
+        combinations = self.get_all_combinations()
+        values = []
+        for combination in combinations:
+            values.append(sum(combination))
+        return values
+        #return self.get_values(self.number)
+
+    # def get_values(self, number: int) -> list:
+    #     if number <= 0:
+    #         return [0]
+    #     if number == 1:
+    #         return [v for v in range(1, self.die.value + 1)]
+    #     else:
+    #         values = []
+    #         for value in range(1, self.die.value + 1):
+    #             values += [value + return_value for return_value in self.get_values(number - 1)]
+    #         return values
+    
+    def get_all_combinations(self) -> list[list[int]]:
+        """Gets all possible combinations of the dice, not considering the modifier.
+
+        Returns:
+            list[list[int]]: A list of lists of dice values.
+        """
+        combinations = self._get_combinations(self.number)
+        if (self.special == SpecialRoll.NONE):
+            pass
+        elif (self.special == SpecialRoll.DROP_HIGHEST):
+            for combination in combinations:
+                for _ in range(self.special_value):
+                    combination.remove(max(combination))
+        elif (self.special == SpecialRoll.DROP_LOWEST):
+            for combination in combinations:
+                for _ in range(self.special_value):
+                    combination.remove(min(combination))
+        else:
+            return NotImplemented
+        return combinations
+
+    def _get_combinations(self, number: int) -> list[list[int]]:
+        """Gets all possible combinations of the specified number of dice, not considering the modifier.
+
+        Args:
+            number (int): The number of dice to get combinations for.
+
+        Returns:
+            list[list[int]]: A list of lists of dice values.
+        """
+        if number <= 0:
+            return [0]
+        if number == 1:
+            return [[v] for v in range(1, self.die.value + 1)]
+        else:
+            if self.die == Dice.D0:
+                return [0]
+            values = []
+            for value in range(1, self.die.value + 1):
+                for combination in self._get_combinations(number - 1):
+                    values.append([value] + combination)
+            return values
+
+class DiceCollection():
+    """
+    Represents a collection of DiceRoll objects.
+    """
+    def __init__(self, dice: list[DiceRoll], special: SpecialRoll = SpecialRoll.NONE,
+        special_value: int = 0) -> None:
+        """Initializes the DiceCollection object with the given parameters.
+
+        Args:
+            dice (list[DiceRoll]): The DiceRolls to use in the collection.
+            special (SpecialRoll, optional): Special modifiers to the roll collection. Defaults to NONE.
+            special_value (int, optional): The value to apply to the selected special. Defaults to 0.
+        """
+        self.dice = dice
+        self.special = special
+        self.special_value = special_value
+    
+    def min_value(self) -> int:
+        """Calculates the minimum possible value of the dice rolls.
+
+        Returns:
+            int: The minimum possible value of the dice rolls.
+        """
+        values = [d.min_value() for d in self.dice]
+        if (self.special == SpecialRoll.NONE):
+            return sum(values)
+        elif (self.special == SpecialRoll.DROP_LOWEST):
+            for _ in range(self.special_value):
+                values.remove(min(values))
+            return sum(values)
+        elif (self.special == SpecialRoll.DROP_HIGHEST):
+            for _ in range(self.special_value):
+                values.remove(max(values))
+            return sum(values)
+        else:
+            return NotImplemented               
+
+    def max_value(self) -> int:
+        """Calculates the maximum possible value of the dice rolls.
+
+        Returns:
+            int: The maximum possible value of the dice rolls.
+        """
+        values = [d.max_value() for d in self.dice]
+        if (self.special == SpecialRoll.NONE):
+            return sum(values)
+        elif (self.special == SpecialRoll.DROP_LOWEST):
+            for _ in range(self.special_value):
+                values.remove(min(values))
+            return sum(values)
+        elif (self.special == SpecialRoll.DROP_HIGHEST):
+            for _ in range(self.special_value):
+                values.remove(max(values))
+            return sum(values)
+        else:
+            return NotImplemented 
+    
+    def get_probability_target(self, target: int) -> float:
+        """Calculates the probability of the dice rolls totaling a given value.
+
+        Args:
+            target (int): The target value.
+
+        Returns:
+            float: The probability of totaling the target value.
+        """
+        if target < self.min_value() or target > self.max_value():
+            return 0.0
+        probabilities = self.get_all_probabilities()
+        return probabilities[target]
+    
+    def get_probability_range(self, min_value: int, max_value: int) -> float:
+        """Calculates the probability of the dice roll landing within a given range.
+
+        Args:
+            min_value (int): The minimum value of the range (inclusive).
+            max_value (int): The maximum value of the range (exclusive).
+
+        Returns:
+            float: The probability of the total roll being within the range.
+        """
+        if min_value < self.min_value() or max_value > self.max_value() or min_value > max_value:
+            return 0.0
+        probabilities = self.get_all_probabilities()
+        return sum([probabilities[p] for p in probabilities.keys() if min_value <= p <= max_value])
+    
+    def get_all_probabilities(self) -> dict[int, float]:
+        """Calculates the probability of rolling each possible value.
+
+        Returns:
+            dict[int, float]: A dictionary where they keys are possible die rolls and the values are the probability of the roll.
+        """
+        possible_values = self.get_all_values()
+        possible_values = [v + self.modifier for v in possible_values]
+        total_possible_outcomes = len(possible_values)
+        outcomes = collections.Counter(possible_values)
+        return {p:outcomes[p] / total_possible_outcomes for p in range(self.min_value(), self.max_value() + 1)}
+    
+    def get_all_values(self) -> list[int]:
+        """Gets all possible values of the dice rolls.
+
+        Returns:
+            list[int]: A list of all possible dice rolls.
+        """
+        combinations = self.get_all_combinations()
+        values = []
+        for combination in combinations:
+            values.append(sum(combination))
+        return values
+    
+    def get_all_combinations(self) -> list[list[int]]:
+        """Gets all possible combinations of the dice.
+
+        Returns:
+            list[list[int]]: A list of lists of dice values.
+        """
+        combinations = self._get_combinations(self.dice)
+        if (self.special == SpecialRoll.NONE):
+            pass
+        elif (self.special == SpecialRoll.DROP_HIGHEST):
+            for combination in combinations:
+                for _ in range(self.special_value):
+                    combination.remove(max(combination))
+        elif (self.special == SpecialRoll.DROP_LOWEST):
+            for combination in combinations:
+                for _ in range(self.special_value):
+                    combination.remove(min(combination))
+        else:
+            return NotImplemented
+        return combinations
+
+    def _get_combinations(self, dice: list[DiceRoll]) -> list[list[int]]:
+        """Gets all possible combinations of the specified dice.
+
+        Args:
+            dice (list[DiceRoll]): The DiceRolls to calculate combinations for.
+
+        Returns:
+            list[list[int]]: A list of lists of dice values.
+        """
         if len(dice) <= 0:
             return [0]
         if len(dice) == 1:
-            return [*range(1, dice[0].value + 1)]
+            return [[v] for v in dice[0].get_all_values()]
         else:
             values = []
-            active_die = dice[0]
-            for value in range(1, active_die+1):
-                values += [value + return_value for return_value in self.get_all_values(dice[1:])]
+            current_dice = dice[0]
+            for value in current_dice.get_all_values():
+                for combination in self._get_combinations(dice[1:]):
+                    values.append([value] + combination)
             return values
-
-            
-    def get_all_probabilities(self) -> dict:
-        pass
-
-def calculate(dice: list, target: int) -> float:
-    pass
